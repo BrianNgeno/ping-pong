d8c2f9f3a6ef9e70811d051a3b7e51ebf3fe3e34
brianngeno <bkn.ngeno@gmail.com>
2018-07-13 13:06:28 +0300

    finished the user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-  $("form").click(function(event){
+//user logic
+$(document).ready(function() {
+  $("form").click(function(event) {
     event.preventDefault();
-    
-  })
-})
+    var userInput = $("input#type").val();
+    var input = parseInt(userInput);
+    nick(input);
+    web.forEach(function(input){
+    ("#user-output").append("<li>" + +"</li>")  });
+  });
+  });
+});
+//business logic
