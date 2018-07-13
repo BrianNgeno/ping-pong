ef089ec33ec608e61be4fa7cc26a2fa35e1cda57
brianngeno <bkn.ngeno@gmail.com>
2018-07-13 15:33:54 +0300

    added business logic to the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,32 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = $("input#type").val();
     var input = parseInt(userInput);
-    nick(input);
-    web.forEach(function(input){
-    ("#user-output").append("<li>" + +"</li>")  });
-  });
+    rick(input);
+    web.forEach(function(input) {
+      $("#user-output").append("<li>" +input +"</li>");
+    });
   });
 });
 //business logic
+var web = [];
+
+function rick(input) {
+  for (index = 1; index <= input; index++){
+   if (index % 15 === 0) {
+      web.push("Ping-Pong");
+    }
+
+  	else if (index % 3 === 0){
+    web.push("Ping");
+  	}
+
+  	else if(index % 5 === 0){
+    web.push("pong");
+  	}
+
+  	else;{
+    web.push(index);
+  }
+  }
+
+}
